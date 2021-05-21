283507f2802fcd4df4e799fc75866eb0933a229e
jlbuild <juliabuildbot@gmail.com>
2021-05-21 16:27:10 +0000

    Blosc2_jll build 2.0.0+0

---
+++ src/Blosc2_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Blosc2_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("Blosc2")
+JLLWrappers.@generate_main_file("Blosc2", UUID("d43303dc-dd0e-56c6-b0a8-331f4c8c9bfb"))
+end  # module Blosc2_jll
