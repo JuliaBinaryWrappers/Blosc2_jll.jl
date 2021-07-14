96ef21f81ad4aa36dead384f3f5fea60f6eae10c
jlbuild <juliabuildbot@gmail.com>
2021-07-14 11:03:06 +0000

    Blosc2_jll build 2.0.2+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -10,7 +10,7 @@ function __init__()
     JLLWrappers.@generate_init_header(Zlib_jll, Zstd_jll, Lz4_jll)
     JLLWrappers.@init_library_product(
         libblosc2,
-        "lib/libblosc2.2.0.1.dylib",
+        "lib/libblosc2.2.0.2.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
