d66de41f2b0fed4fcf8e2a0da7d25f1f4e7887fe
jlbuild <juliabuildbot@gmail.com>
2026-02-03 21:55:02 +0000

    Blosc2_jll build 202.2300.0+0

--- src/Blosc2_jll.jl
+++ src/Blosc2_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Blosc2")
-JLLWrappers.@generate_main_file("Blosc2", UUID("d43303dc-dd0e-56c6-b0a8-331f4c8c9bfb"))
+JLLWrappers.@generate_main_file("Blosc2", Base.UUID("d43303dc-dd0e-56c6-b0a8-331f4c8c9bfb"))
 end  # module Blosc2_jll
