64ed8da79b3b39c4c993a2b5fd5bd6a99b28ab6a
jlbuild <juliabuildbot@gmail.com>
2026-04-29 15:36:17 +0000

    Blosc2_jll build 300.0.0+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -2,15 +2,15 @@
 export libblosc2
 
 using Lz4_jll
-using Zlib_jll
+using ZlibNG_jll
 using Zstd_jll
 JLLWrappers.@generate_wrapper_header("Blosc2")
-JLLWrappers.@declare_library_product(libblosc2, "@rpath/libblosc2.7.dylib")
+JLLWrappers.@declare_library_product(libblosc2, "@rpath/libblosc2.8.dylib")
 function __init__()
-    JLLWrappers.@generate_init_header(Lz4_jll, Zlib_jll, Zstd_jll)
+    JLLWrappers.@generate_init_header(Lz4_jll, ZlibNG_jll, Zstd_jll)
     JLLWrappers.@init_library_product(
         libblosc2,
-        "lib/libblosc2.2.23.1.dylib",
+        "lib/libblosc2.3.0.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
--- src/wrappers/aarch64-linux-gnu.jl
+++ src/wrappers/aarch64-linux-gnu.jl
@@ -2,12 +2,12 @@
 export libblosc2
 
 using Lz4_jll
-using Zlib_jll
+using ZlibNG_jll
 using Zstd_jll
 JLLWrappers.@generate_wrapper_header("Blosc2")
-JLLWrappers.@declare_library_product(libblosc2, "libblosc2.so.7")
+JLLWrappers.@declare_library_product(libblosc2, "libblosc2.so.8")
 function __init__()
-    JLLWrappers.@generate_init_header(Lz4_jll, Zlib_jll, Zstd_jll)
+    JLLWrappers.@generate_init_header(Lz4_jll, ZlibNG_jll, Zstd_jll)
     JLLWrappers.@init_library_product(
         libblosc2,
         "lib/libblosc2.so",
